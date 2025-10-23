d73238adeed64e433338adf5ae1d3ab8aecec4b5
Rodrigo Caetano Braz Da Costa <rodcaeta@c1r2s11.42lisboa.com>
2025-10-23 17:54:16 +0100

    ft_memmove.c

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,30 @@
 /*   By: rodcaeta <rodcaeta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 12:02:30 by rodcaeta          #+#    #+#             */
-/*   Updated: 2025/10/21 11:31:44 by rodcaeta         ###   ########.fr       */
+/*   Updated: 2025/10/23 17:53:29 by rodcaeta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t len)
+{
+	size_t			counter;
+	unsigned char	*temp_dest;
+	unsigned char	*temp_src;
+
+	if (!dest && !src)
+		return (NULL);
+	temp_src = (unsigned char *) src;
+	temp_dest = (unsigned char *) dest;
+	counter = 0;
+	if (temp_dest > temp_src)
+		while (len-- > 0)
+			temp_dest[len] = temp_src[len];
+	else
+	{
+		while (counter++ > 0)
+			temp_dest[counter] = temp_src[counter];
+	}
+	return (dest);
+}
