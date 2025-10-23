2b7520b5d3500ba017bab1643450cd89069968b1
Rodrigo Caetano Braz Da Costa <rodcaeta@c1r2s11.42lisboa.com>
2025-10-23 18:02:08 +0100

    main

--- Libft/main.c
+++ Libft/main.c
@@ -6,7 +6,7 @@
 /*   By: rodcaeta <rodcaeta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 12:00:51 by rodcaeta          #+#    #+#             */
-/*   Updated: 2025/10/21 14:11:38 by rodcaeta         ###   ########.fr       */
+/*   Updated: 2025/10/23 17:52:01 by rodcaeta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,40 @@
 
 int	main(void)
 {
-	char a [] = "rodrigo";
-	char b [] = "caetano";
+	char	a[8] = "rodrigo";
+	char	b[8] = "caetano";
+	char buffer[10] = "palmeiras";
+	char src[] = "Palmeiras";
+	char dest[50];
 
-	printf ("Teste ATOI = %d\n", ft_atoi("42"));
 	printf("Teste IsAlpha (expected 1) = %d\n", ft_isalpha(65));
 	printf("Teste IsAlpha (expected 0)= %d\n", ft_isalpha(49));
+	printf("---------------------\n");
 	printf("Teste IsASCII (expected 1)= %d\n", ft_isascii(112));
 	printf("Teste IsASCII (expected 0)= %d\n", ft_isascii(129));
- 	printf("Teste strlen (counter string) = %zu\n", ft_strlen("rodrigo"));	
+ 	printf("---------------------\n");
+	printf("Teste strlen (counter string) = %zu\n", ft_strlen("rodrigo"));
+	printf("---------------------\n");	
 	printf("Teste strncmp (Compare N strings)= %d\n", ft_strncmp(a, b, 7));
-	
-	/*
+	printf("---------------------\n");
+/* 	ft_memset(b, '*', 7);
+	printf("%s\n", b); */
+	printf("BZERO buffer test before = %s\n", buffer);
+	ft_bzero(buffer, 6);
+	printf("BZERO buffer test after = %s\n", buffer + 7);
+		printf("---------------------\n");
+	ft_memcpy(dest, src, ft_strlen(src) + 1);
+	printf("MEMSET source :%s\n", src);
+ 	printf("MEMSET destnation :%s\n", dest);
+	printf("---------------------\n");
+    char overlap[] = "palmeiras";
+    printf("MEMMOVE antes (sobreposição): %s\n", overlap);
+    ft_memmove(overlap + 5, overlap, 5);
+    printf("MEMMOVE depois (sobreposição): %s\n", overlap);
+/*	printf("%d\n", );
 	printf("%d\n", );
 	printf("%d\n", );
-	printf("%d\n", ); */
+	printf("%d\n", );
+	printf("%d\n", );
+*/
 }
