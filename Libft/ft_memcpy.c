073e81afd94e0e69e6926bbd2e2f2c5395e07858
Rodrigo Caetano Braz Da Costa <rodcaeta@c1r2s11.42lisboa.com>
2025-10-23 16:59:16 +0100

    ft_memcpy.c|ft_memset.c

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -5,8 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rodcaeta <rodcaeta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/20 12:02:28 by rodcaeta          #+#    #+#             */
-/*   Updated: 2025/10/21 11:31:46 by rodcaeta         ###   ########.fr       */
+/*   Created: 2025/10/23 16:28:11 by rodcaeta          #+#    #+#             */
+/*   Updated: 2025/10/23 16:51:59 by rodcaeta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*temp_src;
+	unsigned char	*temp_dest;
+
+	if (dest == (void *)0 && src == (void *)0)
+		return (dest);
+	temp_dest = (unsigned char *) dest;
+	temp_src = (unsigned char *) src;
+	while (n > 0)
+	{
+		*(temp_dest++) = *(temp_src++);
+		n--;
+	}
+	return (dest);
+}
--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,7 +6,21 @@
 /*   By: rodcaeta <rodcaeta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 12:02:31 by rodcaeta          #+#    #+#             */
-/*   Updated: 2025/10/21 11:31:42 by rodcaeta         ###   ########.fr       */
+/*   Updated: 2025/10/23 13:39:46 by rodcaeta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t n)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *) b;
+	while (n > 0)
+	{
+		*ptr++ = (unsigned char) c;
+		n--;
+	}
+	return (b);
+}
