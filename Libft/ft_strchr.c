3f772a011278ca590177047ff14d763e85f6e4de
Rodrigo Caetano Braz Da Costa <rodcaeta@c2r7s1.42lisboa.com>
2025-10-24 15:45:08 +0100

    strchr

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -6,7 +6,26 @@
 /*   By: rodcaeta <rodcaeta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 12:02:32 by rodcaeta          #+#    #+#             */
-/*   Updated: 2025/10/21 11:31:40 by rodcaeta         ###   ########.fr       */
+/*   Updated: 2025/10/24 15:43:00 by rodcaeta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*strchr(const char *s, int c)
+{
+	unsigned int	i;
+	char			c2;
+
+	c2 = (char) c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c2)
+			return ((char *) &s[i]);
+		i++;
+	}
+	if (s[i] == c2)
+		return ((char *) &s[i]);
+	return (NULL);
+}
