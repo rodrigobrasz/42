0ea3efea81778dacc400a9cb0ce603d7dbf525e8
Rodrigo Caetano Braz Da Costa <rodcaeta@c2r8s1.42lisboa.com>
2025-10-26 18:58:38 +0000

    Uptaded MAKEFILE/LIBFT | 26/10/2025

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rodcaeta <rodcaeta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 12:20:09 by rodcaeta          #+#    #+#             */
-/*   Updated: 2025/10/26 17:09:54 by rodcaeta         ###   ########.fr       */
+/*   Updated: 2025/10/26 18:57:00 by rodcaeta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,16 @@ int	ft_isdigit(int c);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void    *ft_memmove(void *dest, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n)
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
