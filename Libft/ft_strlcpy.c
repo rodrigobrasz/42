323e4b08db3c4cdc28315b5d599219cdb70c37ba
Rodrigo Caetano Braz Da Costa <rodcaeta@c2r8s3.42lisboa.com>
2025-10-27 13:39:31 +0000

    strlcpy | 27/10/2025

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -6,7 +6,40 @@
 /*   By: rodcaeta <rodcaeta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 12:02:35 by rodcaeta          #+#    #+#             */
-/*   Updated: 2025/10/21 11:31:35 by rodcaeta         ###   ########.fr       */
+/*   Updated: 2025/10/27 13:36:33 by rodcaeta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t		ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	while (src[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+int main() 
+{
+    char src[] = "rodrigo";
+    char dest[3] = "rod";
+
+    int longueur = ft_strlcpy(dest, src, sizeof(dest));
+
+    printf("%s\n", src);
+    printf("%s\n", dest);
+    printf("%d\n", longueur);
+
+    return 0;
+}
