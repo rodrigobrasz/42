05a49d985418c19f2c0c4f6a155cf76235e73dc8
Rodrigo Caetano Braz Da Costa <rodcaeta@c1r2s11.42lisboa.com>
2025-10-28 13:15:02 +0000

    ft_strdup.c | 1st - 28/10/2025

---
+++ Libft/ft_strdup.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodcaeta <rodcaeta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 11:08:38 by rodcaeta          #+#    #+#             */
+/*   Updated: 2025/10/28 12:18:39 by rodcaeta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	size_t			i;
+	char	*dest;
+
+	i = 0;
+	dest = (char *) malloc(sizeof(char) * (strlen(s)) + 1);
+	if (!dest)
+		return (NULL);
+	while (s[i])
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char a[] = "+_dsadas231312dadas123_)";
+
+	printf("%s", ft_strdup(a));
+	return 0;
+}
