b51329659aa0fae5a28e8aeeef851f758e5961ce
Rodrigo Caetano Braz Da Costa <rodcaeta@c2r5s3.42lisboa.com>
2025-11-03 17:13:07 +0000

    ft_putendl_fd.c | 1st - 03/11/2025

---
+++ Libft/ft_putendl_fd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodcaeta <rodcaeta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 13:37:49 by rodcaeta          #+#    #+#             */
+/*   Updated: 2025/11/03 13:46:27 by rodcaeta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write (fd, &s[i], 1);
+		i++;
+	}
+	write (fd, "\n", 1);
+}
+
+/* int main(void)
+{
+	int i = 1;
+	char a[] = "rodrigo";
+	ft_putendl_fd(a, i);
+	return 0;
+} */
