06857a36d7e536eafadf55e58dc2d15d29bde3a2
Rodrigo Caetano Braz Da Costa <rodcaeta@c2r7s8.42lisboa.com>
2025-11-09 14:13:47 +0000

    final

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,86 +6,100 @@
 /*   By: rodcaeta <rodcaeta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 17:05:40 by rodcaeta          #+#    #+#             */
-/*   Updated: 2025/11/08 18:02:03 by rodcaeta         ###   ########.fr       */
+/*   Updated: 2025/11/09 14:08:09 by rodcaeta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	*ft_freesplit(char **s)
+static void	*ft_freesplit(char **str)
 {
 	size_t	i;
 
 	i = 0;
-	while (s[i])
+	while (str[i])
 	{
-		free (s[i]);
+		free (str[i]);
 		i++;
 	}
-	free (s);
+	free (str);
 	return (NULL);
 }
 
-static char	*ft_fill_split(const char *s, char c)
+static char	*ft_fill_split(const char *str, char delim)
 {
 	int	i;
 
 	i = 0;
-	while (s[i])
+	while (str[i])
 	{
-		while (s[i] && s[i] != c)
+		while (str[i] && str[i] != delim)
 			i++;
-		return (ft_substr(s, 0, i));
+		return (ft_substr(str, 0, i));
 	}
 	return (NULL);
 }
 
-static size_t	ft_count_words(const char *s, int c)
+static size_t	ft_count_words(const char *str, int delim)
 {
 	size_t	counter;
 	size_t	i;
 
 	i = 0;
 	counter = 0;
-	while (s[i])
+	while (str[i])
 	{
-		while (s[i] && s[i] == c)
+		while (str[i] && str[i] == delim)
 			i++;
-		if (s[i] && s[i] != c)
+		if (str[i] && str[i] != delim)
 		{
 			counter++;
-			while (s[i] && s[i] != c)
+			while (str[i] && str[i] != delim)
 				i++;
 		}
 	}
 	return (counter);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(char const *str, char delim)
 {
 	char	**result;
-	int		word;
+	int		word_count;
 	int		i;
 	int		j;
 
-	i = 0;
+	i = -1;
 	j = 0;
-	if (!s)
+	if (!str)
 		return (NULL);
-	word = ft_count_words(s, c);
-	result = malloc(sizeof(char *) * (word + 1));
+	word_count = ft_count_words(str, delim);
+	result = ft_calloc(word_count + 1, sizeof(char *));
 	if (!result)
 		return (NULL);
-	while (i < word)
+	while (++i < word_count)
 	{
-		while (s[j] && s[j] == c)
+		while (str[j] && str[j] == delim)
 			j++;
-		result[i] = ft_fill_split(s + j, c);
+		result[i] = ft_fill_split(str + j, delim);
 		if (!result)
 			return (ft_freesplit(result), NULL);
-		while (s[j] && s[j] != c)
+		while (str[j] && str[j] != delim)
 			j++;
 	}
 	result[i] = NULL;
 	return (result);
 }
+
+/* int	main(void)
+{
+	char	**split;
+	const char	*str = "Sphynx of Black Quartz, Judge My Vow!";
+	const char	*str2 = "                Sphynx of Black Quartz, Judge My Vow!";
+	const char	*str3 = "of  Quartz, Judge My Vow            !               ";
+
+	split = ft_split(str, ' ');
+	split = ft_split(str2, ' ');
+	split = ft_split(str3, ' ');
+	(void) split;
+	return (0);
+} */
